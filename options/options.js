aa6da9bc2acfe9b3d3cbf4c22192f4b50333e346
Crowdbotics <team@crowdbotics.com>
2023-03-02 18:21:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-dew-39198",
+  url: "https://twilight-dew-39198.botics.co",
+  api: "https://twilight-dew-39198.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
